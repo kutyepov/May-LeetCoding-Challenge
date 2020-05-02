c2e19d6b576f49a59eecce016eecc97a7c6ba902
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-02 10:26:27 -0400

    feat(day2): implements unit tests

---
+++ src/jewels-and-stones/index.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import numJewelsInStones from './index';
+
+describe('firstBadVersion', () => {
+  it('should find three jewels', () => {
+    expect(numJewelsInStones('aA', 'aAAbbbb')).to.be.equal(3);
+  });
+  it('should find zero jewels', () => {
+    expect(numJewelsInStones('z', 'ZZ')).to.be.equal(0);
+    expect(numJewelsInStones('z', '')).to.be.equal(0);
+    expect(numJewelsInStones('', 'AA')).to.be.equal(0);
+  });
+});
---
+++ src/jewels-and-stones/index.ts
@@ -0,0 +1,5 @@
+export default function numJewelsInStones(J: string, S: string): number {
+  let jewels: number = 0;
+
+  return jewels;
+}
