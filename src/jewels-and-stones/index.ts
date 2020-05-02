3de977571f1504631867a4a06fdd5efaaef1f86c
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-02 10:26:51 -0400

    feat(day2): implements soluton

--- src/jewels-and-stones/index.ts
+++ src/jewels-and-stones/index.ts
@@ -1,5 +1,10 @@
 export default function numJewelsInStones(J: string, S: string): number {
   let jewels: number = 0;
-
+  const set: Set<string> = new Set(J);
+  for (let ch of S) {
+    if (set.has(ch)) {
+      jewels++;
+    }
+  }
   return jewels;
 }
