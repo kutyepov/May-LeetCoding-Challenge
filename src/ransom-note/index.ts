17052350850e71fe068d2975b076ee22c00d8053
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-03 11:16:19 -0400

    feat(day3): adds solution

--- src/ransom-note/index.ts
+++ src/ransom-note/index.ts
@@ -2,5 +2,24 @@ export default function canConstruct(
   ransomNote: string,
   magazine: string
 ): boolean {
+  const map: Map<string, number> = new Map();
+
+  for (let character of magazine) {
+    const count: number | undefined = map.get(character);
+    if (count === undefined) {
+      map.set(character, 1);
+    } else {
+      map.set(character, count + 1);
+    }
+  }
+
+  for (let character of ransomNote) {
+    const count: number | undefined = map.get(character);
+    if (!count) {
+      return false;
+    }
+    map.set(character, count - 1);
+  }
+
   return true;
 }
