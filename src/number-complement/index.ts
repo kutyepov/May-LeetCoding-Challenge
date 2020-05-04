851005ea6e77e50c9eeec34c7e657585153ad7e4
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-04 12:43:16 -0400

    feat(day4): naive solution

--- src/number-complement/index.ts
+++ src/number-complement/index.ts
@@ -1,3 +1,10 @@
 export default function findComplement(num: number): number {
-  return 0;
+  const binary: string = num.toString(2);
+  let resultStr: string = '';
+
+  for (let n of binary) {
+    resultStr += Number(!Number(n)).toString();
+  }
+
+  return parseInt(resultStr, 2);
 }
