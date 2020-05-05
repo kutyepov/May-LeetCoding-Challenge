78847e174e124c1095da418a600e5beff137efb8
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-05 11:49:29 -0400

    feat(day5): O(n) / O(1) hashmap solution

--- src/first-unique-character-in-a-string/index.ts
+++ src/first-unique-character-in-a-string/index.ts
@@ -1,3 +1,40 @@
 export default function firstUniqChar(str: string): number {
-  return -1;
+  /*
+    Build a map, where character is a `key` 
+    and value is its position in a string.
+    If value is `null`, then character was
+    encountered more than once.
+  */
+  const map: Map<string, number> = new Map();
+
+  for (let i: number = 0; i < str.length; i++) {
+    const character = str[i];
+    if (map.has(character)) {
+      // Found a duplicate character in a string
+      map.set(character, null);
+    } else {
+      // Presumably unique character.
+      // Saving its position in string
+      map.set(character, i);
+    }
+  }
+
+  let pos: number = null;
+
+  /*
+    Iterate over all values in map and find
+    an element with lowest index
+   */
+
+  for (let [_, index] of map) {
+    if (pos === null && index !== null) {
+      pos = index;
+    }
+
+    if (pos !== null && index !== null) {
+      pos = Math.min(index, pos);
+    }
+  }
+
+  return pos === null ? -1 : pos;
 }
