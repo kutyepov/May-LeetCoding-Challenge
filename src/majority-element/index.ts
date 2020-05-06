828e23e4db4e5234a12b9dc3b72df890d1926237
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-06 10:41:17 -0400

    feat(day6): O(n) / O(n) solution

--- src/majority-element/index.ts
+++ src/majority-element/index.ts
@@ -1,3 +1,24 @@
 export default function majorityElement(nums: Array<number>): number {
-  return 0;
+  /* 
+  Frequency Map - key/value pair where 
+    key is an element in `nums` array and
+    value - how many times an element appears in `nums`
+  */
+  const frequencyMap = {};
+
+  // build frequency map
+  for (let n of nums) {
+    // ~~ -> is used to cast `undefined` to `0`
+    frequencyMap[n] = ~~frequencyMap[n] + 1;
+  }
+
+  const threshold = Math.floor(nums.length / 2);
+
+  // iterate through the frequency map and return
+  // an element that appears more than |_(n / 2)_| times.
+  for (let key in frequencyMap) {
+    if (frequencyMap[key] > threshold) {
+      return Number(key);
+    }
+  }
 }
