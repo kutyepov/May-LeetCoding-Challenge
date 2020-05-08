2258a0fb032d41cbbe255b6335f6be8e2f14f171
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-08 17:24:51 -0400

    feat(day7): adds tree node

---
+++ src/_helpers/treenode/index.ts
@@ -0,0 +1,12 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(value: number) {
+    this.val = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export default TreeNode;
