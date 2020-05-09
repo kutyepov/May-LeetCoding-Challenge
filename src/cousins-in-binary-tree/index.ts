4143b88658a67d097c52003618d43ccd17513967
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-09 16:05:35 -0400

    feat(day7): adds test

---
+++ src/cousins-in-binary-tree/index.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import TreeNode from '../_helpers/treenode';
+
+export default function areCousins(
+  root: TreeNode,
+  x: number,
+  y: number
+): boolean {
+  return false;
+}
