5201931dbc897665364630db619a27705bd8cc1d
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-09 16:50:16 -0400

    feat(day9): solution

--- src/valid-perfect-square/index.ts
+++ src/valid-perfect-square/index.ts
@@ -1,3 +1,22 @@
 export default function isPerfectSquare(num: number): boolean {
-  return false;
+  if (num === 1) {
+    return true;
+  }
+
+  let x: number = num;
+  let y: number = 1;
+  let precision: number = 0.99;
+
+  // Babylonian method of computing square root
+  // https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+  // in this case, it uses low precision, since it is not needed for perfect squares
+  while (x - y > precision) {
+    x = (x + y) / 2;
+    y = num / x;
+  }
+
+  // cast x to int
+  x = ~~x;
+
+  return x * x === num;
 }
