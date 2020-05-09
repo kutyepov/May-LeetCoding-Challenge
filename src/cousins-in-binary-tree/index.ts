6cfc391edcdd438c630a03a3d01632c7ff49e06a
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-09 16:05:58 -0400

    feat(day7): solution

--- src/cousins-in-binary-tree/index.ts
+++ src/cousins-in-binary-tree/index.ts
@@ -6,5 +6,51 @@ export default function areCousins(
   x: number,
   y: number
 ): boolean {
-  return false;
+  const q = [root];
+
+  let isXfound = false;
+  let isYfound = false;
+
+  let xLevel = 0;
+  let yLevel = 0;
+
+  let xParent = root;
+  let yParent = root;
+
+  let currentLevel = 0;
+
+  // modified breadths first search
+  // a.k.a. level order traversal
+  while (q.length > 0) {
+    let width = q.length;
+    currentLevel++;
+    while (width > 0) {
+      const node = q.pop();
+      if (node.left?.val === x || node.right?.val === x) {
+        xParent = node;
+        xLevel = currentLevel;
+        isXfound = true;
+      }
+      if (node.left?.val === y || node.right?.val === y) {
+        yParent = node;
+        yLevel = currentLevel;
+        isYfound = true;
+      }
+
+      if (isXfound && isYfound) {
+        break;
+      }
+
+      node.left && q.unshift(node.left);
+      node.right && q.unshift(node.right);
+      width--;
+    }
+
+    // doesn't make sense to keep searching if either 'x' or 'y' was found
+    // both values should have been found on the same level to be cousins
+    if (isXfound || isYfound) {
+      break;
+    }
+  }
+  return isYfound && isXfound && xLevel === yLevel && xParent !== yParent;
 }
