721631062d532ff1ed352d836e0784b21f1217c3
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-15 12:25:32 -0400

    feat(day15): solution

--- src/maximum-sum-circular-subarray/index.ts
+++ src/maximum-sum-circular-subarray/index.ts
@@ -1,3 +1,36 @@
+/*
+  Credit: https://leetcode.com/articles/maximum-sub-circular-subarray/
+*/
+
+function subarrayMaxSum(
+  array: number[],
+  start: number,
+  end: number,
+  multiplier: number
+): number {
+  /* implementation of Kadane's algorithm
+   * https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
+   */
+  let max: number = Number.MIN_SAFE_INTEGER;
+  let currentMax: number = max;
+
+  for (let i = start; i < end; i++) {
+    currentMax = multiplier * array[i] + Math.max(0, currentMax);
+    max = Math.max(currentMax, max);
+  }
+
+  return max;
+}
+
 export default function maxSubarraySumCircular(A: number[]): number {
-  return 0;
+  let sum = 0;
+  for (const num of A) {
+    sum += num;
+  }
+
+  return Math.max(
+    subarrayMaxSum(A, 0, A.length, 1),
+    sum + subarrayMaxSum(A, 1, A.length, -1),
+    sum + subarrayMaxSum(A, 0, A.length - 1, -1)
+  );
 }
