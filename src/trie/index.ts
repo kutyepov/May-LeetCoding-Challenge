6b9d3f4904371e3608249a7e7e17eee6c1e19172
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-14 20:24:20 -0400

    feat(day14): adds tests

---
+++ src/trie/index.ts
@@ -0,0 +1,15 @@
+class Trie {
+  constructor() {}
+
+  insert(word: string): void {}
+
+  search(word: string): boolean {
+    return true;
+  }
+
+  startsWith(prefix: string): boolean {
+    return true;
+  }
+}
+
+export default Trie;
