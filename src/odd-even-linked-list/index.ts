292218cf24775d2b70b58f50a328408b0b7ffab6
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-16 14:15:18 -0400

    feat(day16): O(n) / O(n) solution

--- src/odd-even-linked-list/index.ts
+++ src/odd-even-linked-list/index.ts
@@ -2,5 +2,31 @@
 import ListNode from '../_helpers/listnode';
 
 export default function oddEvenList(head: ListNode) {
-  return null;
+  if (head === null || head.next === null) {
+    return head;
+  }
+
+  let odd = new ListNode(head.val);
+  let even = new ListNode(head.next.val);
+  let evenHead = even;
+  let oddHead = odd;
+  let i = 3;
+
+  let current = head.next.next;
+
+  while (current !== null) {
+    if (i % 2 === 0) {
+      //even
+      even.next = new ListNode(current.val);
+      even = even.next;
+    } else {
+      odd.next = new ListNode(current.val);
+      odd = odd.next;
+    }
+    i++;
+    current = current.next;
+  }
+
+  odd.next = evenHead;
+  return oddHead;
 }
