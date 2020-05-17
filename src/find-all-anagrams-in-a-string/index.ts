d6ac83eb0b77e7f9e25f8e9af146961f8fae4ad1
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-17 11:56:39 -0400

    feat(day17): brute force solution

--- src/find-all-anagrams-in-a-string/index.ts
+++ src/find-all-anagrams-in-a-string/index.ts
@@ -1,5 +1,21 @@
 export default function findAnagrams(s: string, p: string): number[] {
   const result: number[] = [];
 
+  let strings: string[] = [];
+  let n = p.length;
+
+  for (let i = 0; i < s.length - (n - 1); i++) {
+    strings.push(s.substr(i, n));
+  }
+
+  p = p.split('').sort().join('');
+
+  for (let i = 0; i < strings.length; i++) {
+    let str = strings[i].split('').sort().join('');
+    if (str === p) {
+      result.push(i);
+    }
+  }
+
   return result;
 }
