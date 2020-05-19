79b4b13d5ed4f71ef163e2f3c24712b7477a1b40
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-19 16:57:41 -0400

    feat(day19): solution

---
+++ src/online-stock-span/index.ts
@@ -0,0 +1,21 @@
+class StockSpanner {
+  stack: [number, number][];
+
+  constructor() {
+    this.stack = [];
+  }
+
+  next(price: number): number {
+    let count: number = 1;
+    const stack = this.stack;
+
+    while (stack.length > 0 && stack[stack.length - 1][0] <= price) {
+      count += stack.pop()[1];
+    }
+
+    stack.push([price, count]);
+    return count;
+  }
+}
+
+export default StockSpanner;
