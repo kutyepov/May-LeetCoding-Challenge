c015b5cda4c5d0b4d9f4cd18d488de6bdde88391
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-20 10:05:25 -0400

    feat(day20): naive O(n) solution

--- src/kth-smallest-element-in-a-bst/index.ts
+++ src/kth-smallest-element-in-a-bst/index.ts
@@ -1,8 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import TreeNode from '../_helpers/treenode';
 
-var kthSmallest = function (root: TreeNode, k: number) {
-  return 1;
-};
+function traverse(node: TreeNode, memo: number[]): void {
+  if (node === null) {
+    return;
+  }
 
-export default kthSmallest;
+  traverse(node.left, memo);
+  memo.push(node.val);
+  traverse(node.right, memo);
+}
+
+export default function kthSmallest(root: TreeNode, k: number): number {
+  const memo = [];
+  traverse(root, memo);
+  return memo[k - 1];
+}
