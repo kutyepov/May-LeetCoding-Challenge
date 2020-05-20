7c2176fd91abcda1076a31941011cf619fc4246b
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-20 10:01:40 -0400

    feat(day20): tests

---
+++ src/kth-smallest-element-in-a-bst/index.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import kthSmallest from './index';
+import TreeNode from '../_helpers/treenode';
+
+describe('kthSmallest', () => {
+  it('should find correct kth element in BST', () => {
+    const root = new TreeNode(5);
+    root.left = new TreeNode(3);
+    root.right = new TreeNode(6);
+    root.left.left = new TreeNode(2);
+    root.left.right = new TreeNode(4);
+    root.left.left.left = new TreeNode(1);
+
+    expect(kthSmallest(root, 1)).to.be.equal(1);
+    expect(kthSmallest(root, 2)).to.be.equal(2);
+    expect(kthSmallest(root, 3)).to.be.equal(3);
+    expect(kthSmallest(root, 4)).to.be.equal(4);
+    expect(kthSmallest(root, 5)).to.be.equal(5);
+    expect(kthSmallest(root, 6)).to.be.equal(6);
+  });
+});
---
+++ src/kth-smallest-element-in-a-bst/index.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import TreeNode from '../_helpers/treenode';
+
+var kthSmallest = function (root: TreeNode, k: number) {
+  return 1;
+};
+
+export default kthSmallest;
