e84364e93b0164e650595736e1aca6fc212d5507
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-22 12:21:36 -0400

    feat(day22): <3 functional programming

--- src/sort-characters-by-frequency/index.ts
+++ src/sort-characters-by-frequency/index.ts
@@ -1,3 +1,10 @@
 export default function frequencySort(s: string) {
-  return s;
+  return Object.entries(
+    s.split('').reduce((frequencies, ch) => {
+      frequencies[ch] = ~~frequencies[ch] + 1;
+      return frequencies;
+    }, <{ string: number }>{})
+  )
+    .sort(([, f1], [, f2]) => f2 - f1)
+    .reduce((str, [ch, freq]) => str + new Array(freq).fill(ch).join(''), '');
 }
