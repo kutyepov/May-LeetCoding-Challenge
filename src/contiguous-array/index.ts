13fc794f77371cef0b0968b2c1f78aa8d177e58a
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-26 15:19:46 -0400

    feat(day26): solution

--- src/contiguous-array/index.ts
+++ src/contiguous-array/index.ts
@@ -1,3 +1,20 @@
 export default function findMaxLength(nums: number[]): number {
-  return 0;
+  const map: { [key: number]: number } = { 0: -1 };
+  let count = 0;
+
+  return nums.reduce((max, num, i) => {
+    if (num === 0) {
+      count--;
+    } else {
+      count++;
+    }
+
+    if (map[count] !== undefined) {
+      max = Math.max(max, i - map[count]);
+    } else {
+      map[count] = i;
+    }
+
+    return max;
+  }, 0);
 }
