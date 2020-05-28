8362fe1511d4fd7bc3da43ace2f5004238a7b479
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-28 13:45:24 -0400

    day(28): O(n) solution

--- src/counting-bits/index.ts
+++ src/counting-bits/index.ts
@@ -1,11 +1,11 @@
 export default function countBits(num: number): number[] {
-  const result: number[] = [];
-  while (num > 0) {
-    result.unshift(
-      Array.from(num.toString(2)).filter((bit) => bit === '1').length
-    );
-    num--;
+  const result: number[] = new Array(num + 1).fill(0);
+  let i = 0;
+
+  while (i++ < num) {
+    const half: number = result[~~(i / 2)];
+    result[i] = i & 1 ? half + 1 : half;
   }
-  result.unshift(0);
+
   return result;
 }
