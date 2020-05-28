0ea10b24df1094f6610c8a5887b6da7580251dc6
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-28 12:40:08 -0400

    feat(day28): O(n * sizeof(num) solution

--- src/counting-bits/index.ts
+++ src/counting-bits/index.ts
@@ -1,3 +1,11 @@
 export default function countBits(num: number): number[] {
-  return [];
+  const result: number[] = [];
+  while (num > 0) {
+    result.unshift(
+      Array.from(num.toString(2)).filter((bit) => bit === '1').length
+    );
+    num--;
+  }
+  result.unshift(0);
+  return result;
 }
