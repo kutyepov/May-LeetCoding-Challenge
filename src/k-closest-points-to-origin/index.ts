ed9ff6bd0e33aa5d6b85c084ba9e62991ea57519
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-30 14:01:53 -0400

    feat(day30): solution

--- src/k-closest-points-to-origin/index.ts
+++ src/k-closest-points-to-origin/index.ts
@@ -1,3 +1,4 @@
-const kClosest = (points: number[][], K: number): number[][] => [[]];
+const kClosest = (points: number[][], K: number): number[][] =>
+  points.sort(([a, b], [c, d]) => a * a + b * b - (c * c + d * d)).slice(0, K);
 
 export default kClosest;
