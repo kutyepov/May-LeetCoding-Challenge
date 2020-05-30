7a05ebc4ee9bb829f6dff892c29435b44f80f35c
Stanislav Kutyepov <s.kutyepov@gmail.com>
2020-05-29 21:02:57 -0400

    feat(day29): solution

--- src/course-schedule/index.ts
+++ src/course-schedule/index.ts
@@ -2,5 +2,26 @@ export default function canFinish(
   numCourses: number,
   prerequisites: number[][]
 ): boolean {
-  return false;
+  const graph: number[][] = new Array(numCourses).fill(null).map(() => []);
+  /* index is course number; value is the number of prerequisites for this course; */
+  const map = new Array(numCourses).fill(0);
+  const courses = [];
+
+  for (const [course, prerequisite] of prerequisites) {
+    graph[prerequisite].push(course);
+    map[course]++;
+  }
+
+  for (let i = 0; i < numCourses; i++) {
+    !map[i] && courses.push(i);
+  }
+
+  while (courses.length > 0) {
+    numCourses--;
+    for (const c of graph[courses.shift()!]) {
+      !--map[c] && courses.push(c);
+    }
+  }
+
+  return !numCourses;
 }
